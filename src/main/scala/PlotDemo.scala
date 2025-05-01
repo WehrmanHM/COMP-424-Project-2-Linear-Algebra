6d387fc50de7c6fc0cffc82514c98d62f64a0a8d
LegoBuilder370 <cjkobulnicky@gmail.com>
2025-05-01 16:53:34 -0400

    fixed cube, I rebuilt the sbt but it should be the same as before

--- src/main/scala/PlotDemo.scala
+++ src/main/scala/PlotDemo.scala
@@ -58,7 +58,7 @@ object PlotDemo extends JFXApp3 {
   // delete if this breaks something, but it shouldn't
   def generateCube(n: Int, sideLength: Double, x: Double, y: Double, z: Double): Array[Pt4D] = {
     // calculate the vertices, right now these are not used
-    val point1 = (sideLength / 2 + x, sideLength / 2 + y, -sideLength / 2 + z, 1.0)
+    /*val point1 = (sideLength / 2 + x, sideLength / 2 + y, -sideLength / 2 + z, 1.0)
     val point2 = (-sideLength / 2 + x, sideLength / 2 + y, -sideLength / 2 + z, 1.0)
     val point3 = (sideLength / 2 + x, -sideLength / 2 + y, -sideLength / 2 + z, 1.0)
     val point4 = (-sideLength / 2 + x, -sideLength / 2 + y, -sideLength / 2 + z, 1.0)
@@ -66,7 +66,7 @@ object PlotDemo extends JFXApp3 {
     val point6 = (-sideLength / 2 + x, sideLength / 2 + y, sideLength / 2, 1.0)
     val point7 = (sideLength / 2 + x, -sideLength / 2 + y, sideLength / 2, 1.0)
     val point8 = (-sideLength / 2 + x, -sideLength / 2 + y, sideLength / 2, 1.0)
-    Array(point1, point2, point3, point4, point5, point6, point8)
+    Array(point1, point2, point3, point4, point5, point6, point8)*/
 
     Array.fill(n) {
       // u and v are random variables that allow us to cover the sphere
@@ -78,29 +78,33 @@ object PlotDemo extends JFXApp3 {
         val x = u * sideLength - sideLength / 2
         val y = v * sideLength - sideLength / 2
         val z = sideLength / 2
+        (x, y, z, 1.0)
       } else if (face == 1) {
         val x = u * sideLength - sideLength / 2
         val y = v * sideLength - sideLength / 2
         val z = - sideLength / 2
+        (x, y, z, 1.0)
       } else if (face == 2) {
         val x = u * sideLength - sideLength / 2
         val z = v * sideLength - sideLength / 2
         val y = sideLength / 2
-      } else if (face == 2) {
+        (x, y, z, 1.0)
+      } else if (face == 3) {
         val x = u * sideLength - sideLength / 2
         val z = v * sideLength - sideLength / 2
         val y = - sideLength / 2
-      } else if (face == 2) {
+        (x, y, z, 1.0)
+      } else if (face == 4) {
         val y = u * sideLength - sideLength / 2
         val z = v * sideLength - sideLength / 2
         val x = sideLength / 2
+        (x, y, z, 1.0)
       } else {
         val y = u * sideLength - sideLength / 2
         val z = v * sideLength - sideLength / 2
         val x = - sideLength / 2
+        (x, y, z, 1.0)
       }
-
-      (x, y, z, 1.0)
     }
   }
 
@@ -158,8 +162,8 @@ object PlotDemo extends JFXApp3 {
       if (now - lastUpdate >= minInterval) {
         //lastUpdate = now
         val n = slider.value.value.toInt
-        val pts = generateSphere(n, 4)
-        // val pts = generateCube(n, 7, 0, 0)
+        //val pts = generateSphere(n, 4)
+        val pts = generateCube(n, 7, 0, 0, 0)
         val M = compositeMatrix
 
         val t0 = System.nanoTime()
