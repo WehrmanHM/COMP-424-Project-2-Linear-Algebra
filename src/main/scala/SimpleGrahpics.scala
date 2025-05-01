a77b360f0b26719076b7e2cf337fff51fddf400b
LegoBuilder370 <cjkobulnicky@gmail.com>
2025-05-01 13:07:44 -0400

    in progress

--- src/main/scala/SimpleGrahpics.scala
+++ src/main/scala/SimpleGrahpics.scala
@@ -11,6 +11,8 @@ import doodle.java2d.*
 // Need the Cats Effect runtime to run everything
 import cats.effect.unsafe.implicits.global
 
+import scala.io.StdIn.readLine
+
 val blackSquare = Image.rectangle(30, 30).fillColor(Color.black)
 val redSquare = Image.rectangle(30, 30).fillColor(Color.red)
 
@@ -29,7 +31,40 @@ val chessboard =
 
 @main
 def draw(): Unit = {
+  var moveText = ""
   while (true) {
+    moveText = readLine()
+    println(s"Cube will rotate to the $moveText")
+
     chessboard.draw()
   }
+}
+
+class PointH(val x: Double, val y: Double, val z: Double) {
+
+  var pArray: Array[Double] = Array();;
+  
+  def this(x: Double, y: Double, z: Double, pArray: Array[Double]) = {
+    this(x, y, z)
+    this.pArray = Array(x, y, z, 1.0)
+  }
+
+  def getPoint(): Array[Double] = {
+    pArray
+  }
+
+}
+
+class Cube (val sideLength: Double, val x: Double, val y: Double) {
+
+
+  var point1 = PointH(sideLength / 2 + x, sideLength / 2 + y, 0)
+  var point2 = PointH(-sideLength / 2 + x, sideLength / 2 + y, 0)
+  var point3 = PointH(sideLength / 2 + x, -sideLength / 2 + y, 0)
+  var point4 = PointH(-sideLength / 2 + x, -sideLength / 2 + y, 0)
+  var point5 = PointH(sideLength / 2 + x, sideLength / 2 + y, sideLength)
+  var point6 = PointH(-sideLength / 2 + x, sideLength / 2 + y, sideLength)
+  var point7 = PointH(sideLength / 2 + x, -sideLength / 2 + y, sideLength)
+  var point8 = PointH(-sideLength / 2 + x, -sideLength / 2 + y, sideLength)
+  var pArray = Array(point1, point2, point3, point4, point5, point6, point8)
 }
